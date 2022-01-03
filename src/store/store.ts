3e5ed9ec984e8fca4bb78cca7ba1d01a8160d910
Kacper Dąbrowski <kacper@couponfollow.com>
2022-01-03 12:09:42 +0100

    Add userSlice to rootStore

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useReducer } from "react";
 import counterReducer from "./counter/counterSlice";
 
 export const rootStore = configureStore({
   reducer: {
     counter: counterReducer,
+    user: useReducer,
   },
 });
 
