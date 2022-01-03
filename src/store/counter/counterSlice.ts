0851a709f75f255970aef3c689f8b8d85f58f9ea
Kacper Dąbrowski <kacper@couponfollow.com>
2022-01-03 10:52:50 +0100

    add tests for counterSlice

--- src/store/counter/counterSlice.ts
+++ src/store/counter/counterSlice.ts
@@ -1,5 +1,9 @@
 import { AnyAction, createSlice, ThunkAction } from "@reduxjs/toolkit";
 
+
+const initialState = {
+    value: 0
+}
 export interface CounterState {
     value: number
 }
@@ -8,9 +12,7 @@ export type defaultCounterThunk = ThunkAction<void, CounterState, unknown, AnyAc
 
 export const counterSlice = createSlice({
     name: "counter",
-    initialState: {
-        value: 0
-    },
+    initialState,
     reducers: {
         increment: state => {
             state.value += 1
@@ -18,22 +20,17 @@ export const counterSlice = createSlice({
         decrement: state => {
             state.value -= 1
         },
-        incrementByValue: (state, action) => {
+        incrementByValue: (state, action: { payload: number }) => {
             state.value += action.payload
         },
-        decrementByValue: (state, action: { payload: { value: number } }) => {
-            state.value -= action.payload.value
+        decrementByValue: (state, action: { payload: number }) => {
+            state.value -= action.payload
         }
     }
 })
 
 export const { incrementByValue, increment, decrementByValue, decrement } = counterSlice.actions
-
-export const incrementAsync = (amount: number): defaultCounterThunk => dispatch => {
-    setTimeout(() => {
-        dispatch(incrementByValue(amount))
-    })
-}
-
 export default counterSlice.reducer;
+
+
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit"
-import counterReducer from './counter/counterSlice'
-
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter/counterSlice";
 
 export const rootStore = configureStore({
-    reducer: {
-        counter: counterReducer
-    }
-})
+  reducer: {
+    counter: counterReducer,
+  },
+});
 
-export type AppState = ReturnType<typeof rootStore.getState>
-export type AppDispatch = typeof rootStore.dispatch
+export type AppState = ReturnType<typeof rootStore.getState>;
+export type AppDispatch = typeof rootStore.dispatch;
